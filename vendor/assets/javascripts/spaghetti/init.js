54e5346c5ec83aa7e53ea15334150c531f16c866
Omar Abdel-Wahab <owahab@gmail.com>
2012-12-16 23:40:58 +0200

    Adding pre and post spaghetti hooks

--- vendor/assets/javascripts/spaghetti/init.js
+++ vendor/assets/javascripts/spaghetti/init.js
@@ -1,22 +1,35 @@
 $(function() {
+  function nearBottomOfPage() {
+    return $(window).scrollTop() > $(document).height() - $(window).height() - 200;
+  }
+  
+  function triggerHooks(hook) {
+    fn = hook + '_spaghetti';
+    console.log('Triggering hook: ' + hook);
+    if (typeof window.fn == 'function') {
+      fireHook(fn);
+    }
+  }
+  
+  function fireHook(func){
+    this[func].apply(this, Array.prototype.slice.call(arguments, 1));
+  }
+
   $('.spaghetti-empty').each(function() {
     $.ajax({
       url: $(this).data('spaghetti-url') + '?page=' + $(this).data('spaghetti-next-page'),
       type: 'get',
       dataType: 'script',
       success: function() {
-        $(window).sausage('draw');
         $(this).removeClass('spaghetti-empty');
       }
     });
   });
-  function nearBottomOfPage() {
-    return $(window).scrollTop() > $(document).height() - $(window).height() - 200;
-  }
-
+  
   $(window).scroll(function(){
     if ($('.spaghetti').data('spaghetti-enabled') == 1) {
       if(nearBottomOfPage()) {
+        triggerHooks('pre');
         $('.spaghetti').data('spaghetti-enabled', 0);
         $('.spaghetti').addClass('spaghetti-loading');
         $.ajax({
@@ -25,6 +38,7 @@ $(function() {
           dataType: 'script',
           success: function() {
             $(window).sausage('draw');
+            triggerHooks('post');
           }
         });
       }
